91ed395cdd3c37edf0bd02259359994f04c4e4ac
Ilai Giloh <igiloh@gmail.com>
2016-04-03 01:16:41 +0300

    Increased frame size

--- src/android/LibraryProject/src/com/google/zxing/client/android/camera/CameraManager.java
+++ src/android/LibraryProject/src/com/google/zxing/client/android/camera/CameraManager.java
@@ -41,10 +41,10 @@ public final class CameraManager {
 
   private static final String TAG = CameraManager.class.getSimpleName();
 
-  private static final int MIN_FRAME_WIDTH = 220;
-  private static final int MIN_FRAME_HEIGHT = 220;
-  private static final int MAX_FRAME_WIDTH = 880;
-  private static final int MAX_FRAME_HEIGHT = 680;
+  private static final int MIN_FRAME_WIDTH = 320;
+  private static final int MIN_FRAME_HEIGHT = 320;
+  private static final int MAX_FRAME_WIDTH = 980;
+  private static final int MAX_FRAME_HEIGHT = 780;
 
   private final Context context;
   private final CameraConfigurationManager configManager;
