24d86895a34b1af25c4fc733aafc94f046830d7c
Ilai Giloh <igiloh@gmail.com>
2016-04-05 00:34:26 +0300

    Revert "Increased frame size"
    This reverts commit 91ed395cdd3c37edf0bd02259359994f04c4e4ac.
    
    I've noticed scanning is actually harder since I've increased the frame
    
    Conflicts:
    	src/android/LibraryProject/src/com/google/zxing/client/android/camera/CameraManager.java

--- src/android/LibraryProject/src/com/google/zxing/client/android/camera/CameraManager.java
+++ src/android/LibraryProject/src/com/google/zxing/client/android/camera/CameraManager.java
@@ -41,10 +41,10 @@ public final class CameraManager {
 
   private static final String TAG = CameraManager.class.getSimpleName();
 
-  private static final int MIN_FRAME_WIDTH = 960;
-  private static final int MIN_FRAME_HEIGHT = 540;
-  private static final int MAX_FRAME_WIDTH = 1000;
-  private static final int MAX_FRAME_HEIGHT = 960;
+  private static final int MIN_FRAME_WIDTH = 220;
+  private static final int MIN_FRAME_HEIGHT = 220;
+  private static final int MAX_FRAME_WIDTH = 880;
+  private static final int MAX_FRAME_HEIGHT = 680;
 
   private final Context context;
   private final CameraConfigurationManager configManager;
